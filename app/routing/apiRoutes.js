0e8b4a3e5f48b7cf52f14a36279edc8650ac221e
Neha Nautiyal <nehahaha@gmail.com>
2019-05-04 13:52:48 -0400

    added survey questions

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,10 +10,19 @@ module.exports = function (app) {
 
     app.get("/api/friends", function (req, res) {
         res.json(friendData);
+        console.log(friendData);
     });
 
     app.post("/api/friends", function (req, res) {
         friendData.push(req.body);
+        console.log(req.body);
         res.json(true);
+    });
+
+    app.get(`/api/friends/match`, function (req, res) {
+        var match = [];
+        var friendDiff = 0;
+
+        
     });
 }
