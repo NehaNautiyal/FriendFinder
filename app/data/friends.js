49f4e66384ad8baf4957bff6b933c0eece2545a2
Neha Nautiyal <nehahaha@gmail.com>
2019-05-03 22:08:25 -0400

    added friends names and photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,87 @@
 var friendResults = [
     {
-        name: "",
+        name: "Tom Cruise",
         photo: "",
+        scores: [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        ]
+    },
+    {
+        name: "Barack Obama",
+        photo: "",
+        scores: [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        ]
+    },
+    {
+        name: "Beyonce",
+        photo: "",
+        scores: [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        ]
+    },
+    {
+        name: "Madonna",
+        photo: "",
+        scores: [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        ]
+    },
+    {
+        name: "Gandhi",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQopFwjU1MSNIa5FjONKP28SagKE1RZP9ne8jjnPmLAmo9d-IP",
+        scores: [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        ]
+    },
+    {
+        name: "Ellen Degeneres",
+        photo: "https://img.nbc.com/sites/nbcunbc/files/files/images/2017/4/13/EllenDegeneres.jpg",
         scores: [
             0,
             0,
