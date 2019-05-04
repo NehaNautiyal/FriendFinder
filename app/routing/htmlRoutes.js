c0dc8a8ba675d04a2b32243d3c9b403dfc95c0f2
Neha Nautiyal <nehahaha@gmail.com>
2019-05-03 22:48:06 -0400

    style changes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,7 +10,6 @@ var path = require('path');
 module.exports = function(app) {
 
 app.get("/", function(req, res) {
-    console.log(__dirname);
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
